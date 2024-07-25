af0f91ea2412067d601a7c9861488c535a01a62b
Johmar Souza <johmar.o.souza@gmail.com>
2024-07-25 20:26:56 -0300

    primeiro commit do curso!

---
+++ desafio_1.py
@@ -0,0 +1,9 @@
+nome= input(str('Qual o seu nome?'))
+
+salario= int(input('Qual o seu salário?'))
+
+taxa = float(input('Qual a taxa do salário?'))
+
+salario_total = salario*taxa
+
+print(f'Olá {nome}! Seu salário total é {salario_total}')
